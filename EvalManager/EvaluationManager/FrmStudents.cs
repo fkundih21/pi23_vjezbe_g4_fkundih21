d8a994befdaff08c0076652914edf72ef371b4f7
fkundih21 <fkundih21@student.foi.hr>
2023-05-15 16:54:01 +0200

    Dodatn Teacher repo

---
+++ EvalManager/EvaluationManager/FrmStudents.cs
@@ -0,0 +1,35 @@
+﻿using EvaluationManager.Models;
+using EvaluationManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManager
+{
+    public partial class dgvStudents : Form
+    {
+        public dgvStudents()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvStudents_Load(object sender, EventArgs e)
+        {
+            Show_Students();
+        }
+
+        private void Show_Students()
+        {
+            List<Student> students = StudentRepository.GetStudents();
+            dgvStudents.DataSource == students;
+        }
+
+    }
+
+}
