ca1976ace2a308880a594a1510d727ea15375bb2
fkundih21 <fkundih21@student.foi.hr>
2023-05-15 15:28:05 +0200

    Implementacija hijerarhije

---
+++ EvalManager/EvaluationManager/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationManager.Models
+{
+    public class Student : Person
+    {
+        public int Grade { get; set; }
+    }
+
+}
